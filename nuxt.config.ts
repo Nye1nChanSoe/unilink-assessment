1e535f3b5829aed95283d1a8a974d87f16c9be22
Nyein Chan Soe <nyeinchansoe.business@gmail.com>
2025-01-25 10:36:42 +0700

    Integrate Tailwindcss4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import Aura from "@primevue/themes/aura";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@primevue/nuxt-module"],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   primevue: {
     autoImport: true,
     options: {

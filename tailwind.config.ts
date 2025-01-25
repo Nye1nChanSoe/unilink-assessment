70b1caa243a9600c353e9fd581b19a3f4bcac40b
Nyein Chan Soe <nyeinchansoe.business@gmail.com>
2025-01-25 12:54:24 +0700

    Use tailwind nuxt module

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{vue,js,ts}",
+    "./components/**/*.{vue,js,ts}",
+    "./layouts/**/*.{vue,js,ts}",
+    "./plugins/**/*.{vue,js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#143257",
+        text: {
+          light: "#97AAC2",
+          gray: "#6B7280",
+          blue: "#1C477B",
+          muted: "#667085",
+          dark: "#101828",
+          lightGray: "#E5E7EB",
+          slate: "#37404C",
+          sky: "#3B82F6",
+          slateAlt: "#37404C",
+        },
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
